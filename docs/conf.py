85513e163326702e9a8b9068e81654e7a8d33267
7sDream <didislover@gmail.com>
2015-07-28 01:17:11 +0800

    finish conf.py of docs

--- docs/conf.py
+++ docs/conf.py
@@ -110,6 +110,8 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
