690fa2f9310b0a94052c31e89c6e42e085903e84
7sDream <didislover@gmail.com>
2015-07-29 00:59:37 +0800

    modify setup.py now, now you can run it in python2.x

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from __future__ import unicode_literals
 import sys
 import os
 import re
+import ast
 
 try:
     from setuptools import setup
 except ImportError:
-    from distutils.core import setup
+    from distutils.coere import setup
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
@@ -18,34 +19,35 @@ if sys.argv[-1] == 'publish':
 
 
 def extract_requirements(filename='requirements.txt'):
-    with open(filename, 'r') as fd:
-        lines = fd.read().split('\n')
+    with open(filename, 'rb') as f_require:
+        lines = f_require.read().decode('utf-8').split('\n')
 
     # ignore comments and empty lines
     return [line for line in lines if line and not line.strip().startswith('#')]
 
 
 def extract_version():
-    version = ''
-    with open('zhihu/__init__.py', 'r') as fd:
-        version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                            fd.read(), re.MULTILINE).group(1)
-
-    if not version:
-        raise RuntimeError('Cannot find version information')
+    with open('zhihu/__init__.py', 'rb') as f_version:
+        ast_tree = re.search(
+            r'__version__ = (.*)',
+            f_version.read().decode('utf-8')
+        ).group(1)
+        if ast_tree is None:
+            raise RuntimeError('Cannot find version information')
+        return str(ast.literal_eval(ast_tree))
 
     return version
 
 
-with open('README.md', 'r') as fd:
-    readme = fd.read()
+with open('README.md', 'rb') as f_readme:
+    readme = f_readme.read().decode('utf-8')
 
-with open('ChangeLog.md', 'r') as fd:
-    changelog = fd.read()
+with open('ChangeLog.md', 'rb') as f_changelog:
+    changelog = f_changelog, f_changelog.read().decode('utf-8')
 
-packages = [
-    'zhihu'
-]
+packages = ['zhihu']
+if sys.version_info < (3, 0):
+    packages = [str(bytearray(package, 'ascii')) for package in packages]
 version = extract_version()
 requires = extract_requirements()
 
@@ -53,7 +55,8 @@ requires = extract_requirements()
 setup(
     name='zhihu-py3',
     version=version,
-    description='A parser of zhihu.com with help of bs4 and requests in python3',
+    description='A parser of zhihu.com '
+                'with help of bs4 and requests in python3',
     long_description='{0}\n\n{1}'.format(readme, changelog),
 
     author='7sDream',
@@ -65,7 +68,7 @@ setup(
 
     install_requires=requires,
     extras_require={
-        'use-lxml': 'lxml'
+        'lxml': 'lxml'
     },
     packages=packages,
     classifiers=[
@@ -76,6 +79,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
