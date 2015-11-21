78f6d35fab3af58697f9ee7544cf4182c0c7d4fa
zz <zz.at.field@gmail.com>
2015-11-21 13:37:50 +0800

    fix issue#38

--- zhihu/answer.py
+++ zhihu/answer.py
@@ -146,7 +146,8 @@ class Answer:
         :rtype: str
         """
         error_answer = self.soup.find('div', id='answer-status')
-        error_answer['class'] = 'zm-editable-content'
+        if error_answer:
+            error_answer['class'] = 'zm-editable-content'
 
         answer_wrap = self.soup.find('div', id='zh-question-answer-wrap')
         content = answer_wrap.find('div', class_='zm-editable-content')
